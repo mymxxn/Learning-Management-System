7277a364cd0cd2faa95a49802adc0b9ff9ed96c2
mymxxn <achujaleel123@gmail.com>
2024-11-29 23:56:18 +0530

    initial setup project

---
+++ lib/view/course_listing_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CourseListingScreen extends StatelessWidget {
+  const CourseListingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/view/user_profile_management_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class U extends StatelessWidget {
+  const U({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
